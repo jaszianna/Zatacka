e9b9620dece983a58aefcec2591b9bc8ca356bc7
Tomasz Suchodolski <286407@pw.edu.pl>
2019-01-09 10:12:39 +0100

    Add spaces :)

--- src/sample/ComputerPlayer.java
+++ src/sample/ComputerPlayer.java
@@ -10,88 +10,81 @@ public class ComputerPlayer extends Player {
 
     public ComputerPlayer(String n, int width, int height, Color color) {
         super(n, width, height, color);
-        LeftTurnStack=0;
-        RightTurnStack=0;
-        MarkedTab=Round.Marked;
+        LeftTurnStack = 0;
+        RightTurnStack = 0;
+        MarkedTab = Round.Marked;
     }
 
     public void Move()
     {
-        //alpha=Math.PI/2;
-        if(LeftTurnStack==0&&RightTurnStack==0)
+        if(LeftTurnStack == 0 && RightTurnStack == 0)
         {
-            double Angle=alpha-(Math.PI/30*25);
-            int direction=-25;
-            int finishdirection=0;
-            double MaxDist=Double.MIN_VALUE;
-            while (direction<26)
+            double Angle = alpha-(Math.PI/30*25);
+            int direction =- 25;
+            int finishdirection = 0;
+            double MaxDist = Double.MIN_VALUE;
+            while (direction < 26)
             {
-                double dist=ComputeMaximumLength(x,1000-y,Angle,0);
-                if(dist>MaxDist)
+                double dist = ComputeMaximumLength(x,1000-y,Angle,0);
+                if(dist > MaxDist)
                 {
-                    MaxDist=dist;
-                    finishdirection=direction;
+                    MaxDist = dist;
+                    finishdirection = direction;
                 }
-                direction++;
-                Angle+=Math.PI/30;
+                direction ++;
+                Angle += Math.PI/30;
             }
-            if(finishdirection<0)RightTurnStack=-finishdirection;
-            if(finishdirection>0)LeftTurnStack=finishdirection;
-
-//            double dleft=ComputeMaximumLength(x,y,alpha-Math.PI/15,0);
-//            double d=ComputeMaximumLength(x,y,alpha,0);
-//            double dright=ComputeMaximumLength(x,y,alpha+Math.PI/15,0);
-//            if(dleft>d && dleft > dright){LeftTurnStack=1;}
-//            else if(dright>d){RightTurnStack=1;}
+            if(finishdirection < 0)RightTurnStack =- finishdirection;
+            if(finishdirection > 0)LeftTurnStack = finishdirection;
         }
     }
 
     public double ComputeMaximumLength(double xPos, double yPos, double angle,int width)
     {
-        angle=-angle;
-        MarkedTab=Round.Marked;
-        double yxRatio=Math.tan(angle);
-        double Xjump=0;
-        if(Math.cos(angle)>=0)
+        angle =- angle;
+        MarkedTab = Round.Marked;
+        double yxRatio = Math.tan(angle);
+        double Xjump = 0;
+        if(Math.cos(angle) >= 0)
         {
-            Xjump=0.3;
+            Xjump = 0.3;
         }
         else
         {
-            Xjump=-0.3;
+            Xjump =- 0.3;
         }
 
 
-        double actxPos=xPos;
-        double actyPos=yPos;
+        double actxPos = xPos;
+        double actyPos = yPos;
 
-        double SumXJumps=0;
-        double SumYJumps=0;
-        while(Math.sqrt((SumXJumps)*(SumXJumps)+(SumYJumps)*(SumYJumps))<15)
+        double SumXJumps = 0;
+        double SumYJumps = 0;
+        while(Math.sqrt((SumXJumps) * (SumXJumps) + (SumYJumps) * (SumYJumps)) < 15)
         {
-            actxPos+=Xjump;
-            actyPos+=Xjump*yxRatio;
-            SumXJumps+=Xjump;
-            SumYJumps+=Xjump*yxRatio;
+            actxPos += Xjump;
+            actyPos += Xjump * yxRatio;
+            SumXJumps += Xjump;
+            SumYJumps += Xjump * yxRatio;
         }
 
         while(true)
         {
-            actxPos+=Xjump;
-            actyPos+=Xjump*yxRatio;
-            int actxPosOnTab=(int)actxPos;
-            int actyPosOnTab=1000-(int)actyPos;
-            if(actxPosOnTab<0 || actyPosOnTab<0 || actxPosOnTab >= MarkedTab.length || actyPosOnTab >= MarkedTab[0].length)break;
+            actxPos += Xjump;
+            actyPos += Xjump*yxRatio;
+            int actxPosOnTab = (int)actxPos;
+            int actyPosOnTab = 1000 - (int)actyPos;
+            if(actxPosOnTab < 0 || actyPosOnTab < 0 || actxPosOnTab >= MarkedTab.length || actyPosOnTab >= MarkedTab[0].length)break;
             if(MarkedTab[actxPosOnTab][actyPosOnTab])break;
         }
-        return Math.sqrt((actxPos-xPos)*(actxPos-xPos)+(actyPos-yPos)*(actyPos-yPos));
+        return Math.sqrt((actxPos - xPos) * (actxPos - xPos) + (actyPos - yPos) * (actyPos - yPos));
     }
 
     @Override
     public Boolean getTurningRight() {
-        if(LeftTurnStack>0)
+        if(LeftTurnStack > 0)
         {
-            LeftTurnStack--;
+            LeftTurnStack --;
             return true;
         }
         return false;
@@ -104,9 +97,9 @@ public class ComputerPlayer extends Player {
 
     @Override
     public Boolean getTurningLeft() {
-        if(RightTurnStack>0)
+        if(RightTurnStack > 0)
         {
-            RightTurnStack--;
+            RightTurnStack --;
             return true;
         }
         return false;
