b4f28d4f9a563e0eff6c7043d0d0ef9a9427f8b1
Mikołaj Pohorecki <mikolaj.pohorecki@gmail.com>
2019-01-17 13:58:42 +0100

    Stop game button

--- src/zatacka/Game.java
+++ src/zatacka/Game.java
@@ -7,14 +7,17 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Game implements Runnable {
     private LinkedList<Player> players;
     private Round round;
     private int maxRoundCount;
     private int roundCount;
+    private final AtomicBoolean run = new AtomicBoolean(true);
     private GraphicsContext gc;
     private ObservableList<Player> highscore;
+
 
     public Game(int maxRoundCount, GraphicsContext graphicsContext) {
         highscore = FXCollections.observableArrayList();
@@ -24,8 +27,14 @@ public class Game implements Runnable {
         roundCount = 0;
     }
 
+    public void stop()
+    {
+        run.set(false);
+    }
+
     public void run() {
-        while (roundCount != maxRoundCount) {
+        while (roundCount != maxRoundCount && run.get() == true)
+        {
             round = new Round(players, gc, 15);
             try {
 
@@ -58,6 +67,7 @@ public class Game implements Runnable {
         players = new LinkedList<Player>();
         roundCount = 0;
         round = null;
+        run.set(true);
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
     }
